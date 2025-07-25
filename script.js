ccf022416705887c762eac7adca149cdef443ef9
Famzy-Dev <all@muhannad.in>
2025-07-26 00:32:58 +0300

    Update script.js
    1

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let users;
+
+fetch("users.json")
+  .then(response => response.json())
+  .then(data => users = data);
+
+function checkPassword() {
+  const inputPassword = document.getElementById('password').value;
+  const errorElem = document.getElementById('error');
+  const dashboard = document.getElementById('dashboard-container');
+  const loginContainer = document.getElementById('login-container');
+
+  const user = Object.keys(users).find(user => users[user] === inputPassword);
+
+  if (user) {
+    document.getElementById('welcome-msg').innerText = `Welcome, ${capitalize(user)}!`;
+    loginContainer.style.display = 'none';
+    dashboard.style.display = 'block';
+  } else {
+    errorElem.innerText = 'Wrong password dumbass.';
+  }
+}
+
+function capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
 
